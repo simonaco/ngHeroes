bbad970604fb3055fe981dbdf034fbe85d8e05f8
simonaco <simona.cotin@gmail.com>
2016-10-23 18:20:03 +0200

    Fix typings issues

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,7 +2,8 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 //https://github.com/angular/angularfire2/issues/234
 /// <reference path="../node_modules/firebase/firebase.d.ts" />
+/// <reference path="../node_modules/@types/hammerjs/index.d.ts" />
 
 declare var System: any;
 //https://github.com/angular/angular-cli/issues/2221
-//declare var require: any;
+declare var require: any;
