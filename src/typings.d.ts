025989e103c84387d8168a72399ba74a18e6c3a8
simonaco <simona.cotin@gmail.com>
2016-10-23 17:42:14 +0200

    Fix issue with Cannot find name require

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,5 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+//https://github.com/angular/angular-cli/issues/2221
+declare var require: any;
