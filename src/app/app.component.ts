3a7643ce91a5388af1c47577c5d02af355e77c0c
simonaco <simona.cotin@gmail.com>
2016-10-23 18:11:42 +0200

    Retrieve list of ngHeroes from firebase and display using md-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  heroes:FirebaseListObservable<any[]>;
+  constructor(af:AngularFire){
+    this.heroes = af.database.list("/heroes");
+  }
 }
