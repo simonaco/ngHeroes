d4cb54c2a10ab2e5002b2aa43ff5a3efdef8a949
simonaco <simona.cotin@gmail.com>
2016-10-23 18:01:48 +0200

    Import AngularFire module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: '<your-database-URL>',
+  storageBucket: '<your-storage-bucket>'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,7 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+//https://github.com/angular/angularfire2/issues/234
+/// <reference path="../node_modules/firebase/firebase.d.ts" />
 
 declare var System: any;
 //https://github.com/angular/angular-cli/issues/2221
